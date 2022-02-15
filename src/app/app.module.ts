5db025d9295de6726ef2ef9e0fe56b2199f174c1
noe <noe.reyes92@yahoo.com>
2022-02-15 10:59:07 -0600

    created gallery, cities, city, nav and post componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CitiesComponent } from './cities/cities.component';
+import { CityComponent } from './city/city.component';
+import { NavComponent } from './nav/nav.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    CitiesComponent,
+    CityComponent,
+    NavComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
