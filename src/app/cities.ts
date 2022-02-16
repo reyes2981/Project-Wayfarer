099042bca51e7b29268fbf535ebe785a5ab2b5f7
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2022-02-16 14:49:37 -0600

    update Part 2

---
+++ src/app/cities.ts
@@ -0,0 +1,49 @@
+export const CITIES = [
+  {
+    id: 1,
+    name: 'Gibraltar',
+    imgUrl: 'assets/images/gibraltar.jpg',
+    posts: [{
+      title: "Fake title 1",
+      author: 'Schmitty1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
+        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+    }, {
+      title: "Fake title 2",
+      author: 'Schmitty2',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
+        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+    }]
+  }, {
+    id: 2,
+    name: 'San Francisco',
+    imgUrl: 'assets/images/sanfran.jpg',
+    posts: [{
+      title: "Fake title 1",
+      author: 'Schmitty1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
+        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+    }, {
+      title: "Fake title 2",
+      author: 'Schmitty2',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
+        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+    }]
+  }, {
+    id: 3,
+    name: 'london',
+    imgUrl: 'assets/images/london.jpg',
+    posts: [{
+      title: "Fake title 1",
+      author: 'Schmitty1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
+        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+    }, {
+      title: "Fake title 2",
+      author: 'Schmitty2',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
+        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+    }]
+  }
+  
+];
