debadd02df9baa6722e628a5e81ccf7429e8d3f1
noe <noe.reyes92@yahoo.com>
2022-02-20 19:56:47 -0600

    added css and the text content annette wrote

--- src/app/cities.ts
+++ src/app/cities.ts
@@ -6,14 +6,14 @@ export const CITIES = [
     country: 'gi',
     posts: [{
       id:1,
-      title: "Fake title 1",
+      title: "Lots of fun attractions",
       author: 'Schmitty1',
-      body: 'Gibraltar post 1'
+      body: 'Although small, Gibraltar is home to many fascinating attractions. From the 35 miles of tunnels that weave through its Rock and its famous Barbary apes, to a quirky old town and the dolphins that swim in its waters.'
     }, {
       id:2,
-      title: "Fake title 2",
+      title: "It is a British Overseas Territory",
       author: 'Schmitty2',
-      body: 'Gibraltar post 2'
+      body: 'While there’s something called the “Rock of Gibraltar,” a huge limestone ridge measuring 1,400 feet tall that dominates all of Gibraltar, it’s not just a rock in the ocean. Instead, Gibraltar is a modern British Overseas Territory located on Spain’s southern coast.'
     }]
   }, {
     id: 2,
@@ -22,16 +22,14 @@ export const CITIES = [
     country: 'us',
     posts: [{
       id:1,
-      title: "Fake title 1",
+      title: "Home to Lots of Attractions",
       author: 'Schmitty1',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
-        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+      body: 'San Francisco is one of the greatest cities in the world. Famous for grand-dame Victorians, cable cars, a dynamic waterfront, and a soaring golden bridge, this city truly has it all.'
     }, {
       id:2,
-      title: "Fake title 2",
+      title: "Lots of outdoor fun",
       author: 'Schmitty2',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
-        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+      body: 'With trend-defining cuisine ranging from Michelin-starred dining to outrageous food trucks; world-renowned symphony, ballet, theater, and opera; plus almost boundless outdoor adventures, San Francisco justifiably stands out as one of the ultimate must-visit cities on any traveler’s wish list'
     }]
   }, {
     id: 3,
@@ -40,16 +38,14 @@ export const CITIES = [
     country: 'uk',
     posts: [{
       id:1,
-      title: "Fake title 1",
+      title: "Who Wants to go See a Show?",
       author: 'Schmitty1',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
-        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+      body: 'Explore London on sightseeing tours, soak up culture at museums and galleries, or choose from the many free things to do. Treat yourself on famous shopping streets or celebrate special occasions at world-class theatre shows, restaurants and bars.'
     }, {
       id:2,
-      title: "Fake title 2",
+      title: "Good Eats in London",
       author: 'Schmitty2',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia, pariatur qui quis quo ullam vel?' +
-        ' Accusamus maxime officia vitae voluptatibus! Doloremque maiores mollitia nobis quisquam. Laboriosam molestiae quod voluptates?'
+      body: 'Let your stomach lead you to delicious restaurants in London or enjoy cocktails and drinks with a meal at pubs and top bars in London. The variety of London food options is mind blowing – sit down to Michelin-starred dining, tuck into street food or hang out at outdoor restaurants.'
     }]
   }
   
